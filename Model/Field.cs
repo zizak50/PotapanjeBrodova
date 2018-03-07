aa4958a7eb3d57292187f64591d2424385cf9705
Perica Jurić <pejuric@vsite.hr>
2018-03-07 21:27:34 +0100

    pjuric

---
+++ Model/Field.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public struct Field
+    {
+
+       public Field(int roow, int collumn) { Row = roow;Column = collumn; }
+
+
+        public readonly int Row;
+        public readonly int Column;
+
+    }
+}
---
+++ Tests/FieldTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace Tests
+{
+    [TestClass]
+    public class FieldTest
+    {
+        [TestMethod]
+        public void Field_RowAndColumnCorrespondToValuesProvidedByConstructor ()
+        {
+            Field f = new Field(3,4);
+            Assert.AreEqual(3, f.Row);
+            Assert.AreEqual(4, f.Column);
+        }
+    }
+}
