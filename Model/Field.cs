619f111c52b630b59c925105ba85fc6595d7ef0e
Tihomir Kuhar <tikuhar@vsite.hr>
2018-03-07 21:24:50 +0100

    Riješen prvi test

---
+++ Model/Field.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public struct Field
+    {
+        public Field (int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
